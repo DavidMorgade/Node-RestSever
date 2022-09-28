133e27756a15a6d83b2270657c73a725e0502678
David Morgade <davidmorgadegil@gmail.com>
2022-09-28 06:36:34 +0200

    Google Sign in

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,9 @@
-const { response } = require('express');
+const { response, json } = require('express');
 const bcryptjs = require('bcryptjs');
 const User = require('../models/user.model');
 const { generateJWT } = require('../helpers/generate-jwt');
+const { googleVerify } = require('../helpers/google.verify');
+const { DefaultTransporter } = require('google-auth-library');
 
 const login = async (req, res = response) => {
   const { correo, password } = req.body;
@@ -42,4 +44,47 @@ const login = async (req, res = response) => {
   }
 };
 
-module.exports = { login };
+const googleSignIn = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { correo, nombre, img } = await googleVerify(id_token);
+
+    // Check if user already has a normal account trying to log with google
+    let user = await User.findOne({ correo });
+
+    if (!user) {
+      //Create user
+      const data = {
+        nombre,
+        correo,
+        password: '**',
+        img,
+        google: true,
+      };
+      user = new User(data);
+      await user.save();
+    }
+    // Check if user in DB is deleted / banned
+    if (!user.state) {
+      return res.status(404).json({
+        msg: 'User blocked, talk with an administrator',
+      });
+    }
+
+    // Generate JWT
+    const token = await generateJWT(user.id);
+
+    res.json({
+      user,
+      token,
+    });
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: "The token couldn't be verified",
+    });
+  }
+};
+
+module.exports = { login, googleSignIn };
